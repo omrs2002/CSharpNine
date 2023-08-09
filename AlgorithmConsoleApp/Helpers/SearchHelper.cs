0b5e4dcdd9470c116e3d25406a9a32ff2221aa30
Omar Abuhadid <omrs2002@yahoo.com>
2023-08-09 17:12:13 +0300

    Add Algorith project

---
+++ AlgorithmConsoleApp/Helpers/SearchHelper.cs
@@ -0,0 +1,61 @@
+﻿namespace AlgorithmConsoleApp.Helpers
+{
+    public static class SearchHelper
+    {
+        public static int BinarySearch(int[] A, int key)
+        {
+            int steps = 0;
+            try
+            {
+                int middle;
+                int left = 0, right = A.Length - 1;
+
+                while (left <= right)
+                {
+                    middle = (left + right) / 2;// find the middle, round the result
+                    if (A[middle] == key) return middle;
+                    if (A[middle] > key) right = middle - 1;
+                    if (A[middle] < key) left = middle + 1;
+                    steps++;
+                }
+                return 0;
+            }
+            catch { return 0; }
+            finally { Console.WriteLine("Steps:" + steps); }
+
+        }
+
+        public static int BinarySearchRecursive(int[] inputArray, int key, int min, int max)
+        {
+            if (min > max) return 0;
+            int mid = (min + max) / 2;
+            if (key == inputArray[mid]) //return ++mid;
+                return mid++;
+
+            if (key < inputArray[mid])
+                return BinarySearchRecursive(inputArray, key, min, mid - 1);
+            else
+                return BinarySearchRecursive(inputArray, key, mid + 1, max);
+
+        }
+
+
+        public static int[] SortByInsertionSorted(int[] inputArray)
+        {
+            int[] A = inputArray;
+            int Hand;
+            for (int i = 1; i < inputArray.Length; i++)
+            {
+                int j = i;
+                while (j >= 1 && A[j - 1] > A[j])
+                {
+                    Hand = A[j]; // exchange current book with left neighbor
+                    A[j] = A[j - 1];
+                    A[j - 1] = Hand;
+                    j = j - 1;
+                }
+            }
+            return A;
+        }
+    }
+}
---
+++ AlgorithmConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using AlgorithmConsoleApp.Helpers;
+
+namespace AlgorithmConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int[] A = new int[10000];
+            for (int i = 0; i < 10000; i++) A[i] = i;
+
+            //var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            int indx = SearchHelper.BinarySearch(A, 70);
+            Console.WriteLine($"result of BinarySearch = {indx}");
+
+            int indx2 = SearchHelper.BinarySearchRecursive(A, 70,0,200);
+
+            Console.WriteLine($"result of BinarySearchRecursive = {indx2}");
+            //watch.Stop();
+            //Console.WriteLine($"Total Execution 1 Time: {watch.ElapsedMilliseconds} ms and result = {indx}");
+            //watch.Reset();
+
+            //watch.Start();
+            //int indx2 = Array.IndexOf(A, 70);
+            //watch.Stop();
+            //Console.WriteLine($"Total Execution 2 Time: {watch.ElapsedMilliseconds} and result = {indx2}");
+
+            int[] notSorted = {12,1,5,2,9};
+            var sortedArray = SearchHelper.SortByInsertionSorted(notSorted);
+            Console.WriteLine(sortedArray.ToList().ToString());
+
+
+            Console.ReadLine();
+        }
+    }
+}
